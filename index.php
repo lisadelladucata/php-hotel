408826d07570bc889a402e782e5fdd401739b237
lisadelladucata <lisa.delladucata@gmail.com>
2025-04-15 20:29:13 +0200

    feat:add-exercise

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
+<?php
+
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +50,31 @@
     <title>PHP-HOTEL</title>
 </head>
 <body>
+    <h1>Lista Hotel</h1>
+    <table border="1" cellpadding="10">
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Descrizione</th>
+          <th>Parcheggio</th>
+          <th>Voto</th>
+          <th>Distanza dal centro</th>
+        </tr>
+      </thead>
+      <tbody>
+    <?php
     
+        foreach ($hotels as $hotel) { 
+          <tr>
+            <td> echo $hotel['name']; </td>
+            <td> echo $hotel['description']; </td>
+            <td> echo $hotel['parking'] ? 'Sì' : 'No'; </td>
+            <td> echo $hotel['vote']; </td>
+            <td> echo $hotel['distance_to_center'];  km</td>
+          </tr>
+         } ?>
+      </tbody>
+    </table>
+    ?>
 </body>
 </html>
